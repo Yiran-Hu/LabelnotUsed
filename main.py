786b8092de8822d98441d39788444629dbcc03d2
Yi-ran Hu <fuyiruan@outlook.com>
2022-12-29 17:50:36 -0600

    Update main.py

--- main.py
+++ main.py
@@ -9,31 +9,27 @@ filename = input('What is your tex or txt file name?\n')
 outputorfile = input('Do you want to make it a file or just output?[y]or[n]\n')
 cited = []
 # find all in-text citation of equations
+cited = []
+# find all in-text citation of equations
+
 for idx, line in enumerate(open(filename)):
   if idx <= 2500:
-    if "\\eqref{" in line:
-      start = line.find("\\eqref{") + len("\\eqref{")
-      end = line.find("}")
-      temp = line[start:end]
-      if temp!="":
-        cited.append(temp)
+    result = re.findall("ref\{(.*?)\}", line)
+    if result:
+      cited+=result
 
-    if "\\ref{" in line:
-      start = line.find("\\ref{") + len("\\ref{")
-      end = line.find("}")
-      temp = line[start:end]
-      if temp!="":
-        cited.append(temp)
 
+#
 labelnotused = []
 # find all labels not used
 for idx, line in enumerate(open(filename)):
   if idx <= 2500:
-    if "\\label{" in line:
-      temp = line[line.find("\label{") + len("\label{"):line.find("}")]
-      if temp!="":
-        if not( temp in cited):
-          labelnotused.append(temp+' in the line '+str(idx+1))
+    labels = re.findall("label\{(.*?)\}", line)
+    for label in labels:
+      if not (label in cited):
+        labelnotused.append(label + ' in the line ' + str(idx + 1))
+
+#
 if outputorfile == 'y':
   text_file = open("labelnotused.txt", "w")
 
